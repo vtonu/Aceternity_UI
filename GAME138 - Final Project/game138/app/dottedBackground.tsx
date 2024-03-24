5062248d88c4674df45c6ccc94dc367327c1526d
vtonu <victor.tonu@yahoo.com>
2024-03-24 11:38:20 -0700

    Made dottedBackground a grid, fixed text & removed hovers for the phases.

--- GAME138 - Final Project/game138/app/dottedBackground.tsx
+++ GAME138 - Final Project/game138/app/dottedBackground.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { TracingBeam } from './tracing-beam';
 
 export function DotBackgroundDemo() {
   return (
     <main className="flex">
-      <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
+      <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative grid items-start">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         {/* <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
@@ -30,77 +29,45 @@ export function DotBackgroundDemo() {
               height={24}
               priority
             /> */}
-              Victor Tonu
+              Victor Tonu &copy; 2024
             </a>
           </div>
         </div>
 
-        {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div> */}
-
         <div className="grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-center">
-          <a
-            href="https://github.com/vtonu"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer">
-            {/* <TracingBeam children={undefined} /> */}
+          {/* Phase One */}
+          <a className="group rounded-lg border border-transparent px-5 py-4 ">
             <h2 className={`mb-3 text-2xl font-semibold`}>
               Phase One{' '}
-              {/* <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span> */}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
             </h2>
             <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Ideas & references.</p>
           </a>
 
-          <a
-            href="https://github.com/vtonu"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer">
+          <a className="group rounded-lg border border-transparent px-5 py-4">
             <h2 className={`mb-3 text-2xl font-semibold`}>
               Phase Two{' '}
-              {/* <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span> */}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
             </h2>
             <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Rough blockout & textures.</p>
           </a>
 
-          <a
-            href="https://github.com/vtonu"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer">
+          <a className="group rounded-lg border border-transparent px-5 py-4 ">
             <h2 className={`mb-3 text-2xl font-semibold`}>
               Phase Three{' '}
-              {/* <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span> */}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
             </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Rough assets & composition.</p>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Assets & composition.</p>
           </a>
 
-          <a
-            href="https://github.com/vtonu"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer">
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Final Scene{' '}
-              {/* <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span> */}
-            </h2>
+          <a className="group rounded-lg border border-transparent px-5 py-4 ">
+            <h2 className={`mb-3 text-2xl font-semibold`}>Final Scene </h2>
             <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
               Final renders & post-processing.
             </p>
