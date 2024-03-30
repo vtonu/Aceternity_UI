a51bb12fd78058a7f2f8026b6a14df4a9f4647cf
vtonu <victor.tonu@yahoo.com>
2024-03-30 14:53:53 -0700

    More optimization for mobile-viewing. v0.6

--- GAME138 - Final Project/game138/app/dottedBackground.tsx
+++ GAME138 - Final Project/game138/app/dottedBackground.tsx
@@ -9,15 +9,15 @@ export function DotBackgroundDemo() {
       <p className="text-center text-5xl font-bold relative bg-clip-text text-transparent bg-gradient-to-b from-neutral-300 to-neutral-500 py-8">
         Art Deco Scene
       </p>
-      <div className="items-center justify-center font-mono text-sm lg:flex gap-8 pb-8">
-        <div className="fixed left-0 top-0 flex w-full justify-center border-b border-emerald-300 bg-emerald-300/10 from-emerald-500/20 backdrop-blur-sm dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-emerald-200 lg:p-4 lg:dark:bg-zinc-800/30">
+      <div className="items-center justify-center font-mono text-sm lg:flex gap-2 pb-8">
+        <div className="flex justify-center border border-emerald-300 bg-emerald-300/10 from-emerald-500/20 backdrop-blur-sm dark:border-neutral-800  dark:from-inherit  w-auto  rounded-md  bg-emerald-200 p-4 dark:bg-zinc-800/30">
           GAME138&nbsp;
           <code className="font-mono font-bold">Final Project</code>
           <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
         </div>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className=" flex p-4 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            className="flex place-items-center gap-2"
             href="https://github.com/vtonu"
             target="_blank"
             rel="noopener noreferrer">
@@ -37,7 +37,7 @@ export function DotBackgroundDemo() {
 
       <div className="flex-col">
         {/* Phase 1 */}
-        <a className="text-center">
+        <a className="text-center ">
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Phase One{' '}
             {/* <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
@@ -47,7 +47,7 @@ export function DotBackgroundDemo() {
           <p className={`text-sm opacity-50`}>
             Ideas & references.<br></br>{' '}
           </p>
-          <div className="flex justify-center pb-8 gap-4">
+          <div className="lg:flex justify-center pb-8 gap-4">
             <Image
               src="/phase1/FloorPlanEuropa.jpg"
               alt="ref"
@@ -97,7 +97,7 @@ export function DotBackgroundDemo() {
           <p className={`text-sm opacity-50`}>
             Rough blockout & textures.<br></br> <br></br>
           </p>
-          <div className="flex justify-center pb-8 gap-4">
+          <div className="lg:flex justify-center pb-8 gap-4">
             <Image src="/phase2/Phase1 - Rough Blockout.jpg" alt="ref" width={775} height={0} />
             <Image src="/phase2/Phase1 - Rough Textures.jpg" alt="ref" width={775} height={0} />
           </div>
@@ -115,7 +115,7 @@ export function DotBackgroundDemo() {
           <p className={` text-sm opacity-50`}>
             Rough assets & composition. <br></br> <br></br>
           </p>
-          <div className="flex justify-center pb-8 gap-4">
+          <div className="lg:flex justify-center pb-8 gap-4">
             <Image
               src="/phase3/blockout.jpg"
               alt="ref"
@@ -149,7 +149,7 @@ export function DotBackgroundDemo() {
           <p className={`text-sm opacity-50 text-balance`}>
             Final renders submitted for the assignment.<br></br> <br></br>
           </p>
-          <div className="flex justify-center pb-8 gap-4">
+          <div className="lg:flex justify-center pb-8 gap-4">
             <Image src="/final/Final1.jpg" alt="ref" width={550} height={0} />
             <Image src="/final/Final2.jpg" alt="ref" width={550} height={0} />
             <Image src="/final/Final3.jpg" alt="ref" width={550} height={0} />
@@ -163,13 +163,17 @@ export function DotBackgroundDemo() {
           <p className={`text-sm opacity-50 text-balance`}>
             Additional polishing in Unity.<br></br> <br></br>
           </p>
-          <div className="flex justify-center pb-8 gap-4">
+          <div className="lg:flex justify-center pb-8 gap-4">
+            <Image src="/extra/scene3.jpg" alt="extra scene 1" width={550} height={0} />
             <Image src="/extra/scene1.jpg" alt="extra scene 1" width={550} height={0} />
             <Image src="/extra/scene2.jpg" alt="extra scene 1" width={550} height={0} />
-            <Image src="/extra/scene3.jpg" alt="extra scene 1" width={550} height={0} />
           </div>
-          <h1 className="text-xs font-medium leading-none pt-4 pb-4 flex justify-center items-center gap-2">
+          <h1 className="text-xs font-medium leading-none pt-4 pb-4 lg:flex grid justify-center items-center gap-2 pointer-events-none">
             Powered by{' '}
+            <button className="px-4 py-2 backdrop-blur-sm border bg-gray-300/10 border-gray-500/20 text-white  text-center rounded">
+              Vercel
+              <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-gray-500 to-transparent" />
+            </button>
             <button className="px-4 py-2 backdrop-blur-sm border bg-blue-300/10 border-blue-500/20 text-white  text-center rounded">
               React
               <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-blue-500 to-transparent" />
